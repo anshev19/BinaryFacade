2f16f920d23ff2d889d30f9c3518110ea1547a12
aleksandr.shevchenko <aleksandr.shevchenko@nexign.com>
2023-03-24 15:15:00 +0300

    Binary facade

---
+++ src/com/company/BinOps.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class BinOps {
+    public String sum(String a, String b) {
+        int arg1 = Integer.parseInt(a, 2);
+        int arg2 = Integer.parseInt(b, 2);
+        int res = arg1 + arg2;
+
+        return Integer.toBinaryString(res);
+    }
+
+    public String mult(String a, String b) {
+        int arg1 = Integer.parseInt(a, 2);
+        int arg2 = Integer.parseInt(b, 2);
+        int res = arg1 * arg2;
+
+        return Integer.toBinaryString(res);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("01", "10"));
+        System.out.println(bins.mult("01", "10"));
+    }
+}
